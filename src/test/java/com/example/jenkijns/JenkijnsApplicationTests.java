f19421f9a59e14958189e236b39f2f295dab3a04
mehdifassifihri <m.fassifihri12@gmail.com>
2023-12-30 12:09:55 +0100

    first commit

---
+++ src/test/java/com/example/jenkijns/JenkijnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkijns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkijnsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
